fec65148f6e1dec4843a9d4b11d2826bee13dc03
Ivan Holikov <laurence4312@gmail.com>
2020-12-20 15:49:16 +0200

    Add model News to news app.

--- news/models.py
+++ news/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class News(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    author_name = models.CharField(max_length=150, blank=False, default='')
+    content = models.CharField(max_length=250, blank=True, default='')
