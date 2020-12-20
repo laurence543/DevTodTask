f618e2561a8ec855243b688eeaddae264311c613
Ivan Holikov <laurence4312@gmail.com>
2020-12-20 16:21:00 +0200

    Update requirements.txt and add corsheaders

--- DevTodTask/settings.py
+++ DevTodTask/settings.py
@@ -40,10 +40,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'home.apps.HomeConfig',
     'news.apps.NewsConfig',
-
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
