20857d47f99bc88dd71a257c775d4d6420a9c416
Ivan Holikov <laurence4312@gmail.com>
2020-12-20 22:15:15 +0200

    Update connections for News model

--- news/models.py
+++ news/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class News(models.Model):
     title = models.CharField(max_length=100, blank=False, default='')
     link = models.CharField(max_length=100, blank=False, default='')
-    votes = models.IntegerField()
+    votes = models.IntegerField(default=0)
     created = models.DateTimeField(auto_now_add=True)
     author_name = models.CharField(max_length=150, blank=False, default='')
 
--- news/serializers.py
+++ news/serializers.py
@@ -5,18 +5,21 @@ from rest_framework import serializers
 class NewsSerializer(serializers.ModelSerializer):
 
     pk = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(max_length=100)
+    link = serializers.CharField(max_length=100)
+    votes = serializers.IntegerField()
     author_name = serializers.CharField(max_length=150)
-    content = serializers.CharField(max_length=250)
 
     def create(self, validated_data):
         return News.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.author_name = validated_data.get('name', instance.author_name)
-        instance.content = validated_data.get('description', instance.content)
+        instance.title = validated_data.get('title', instance.title)
+        instance.link = validated_data.get('link', instance.link)
+        instance.author_name = validated_data.get('author_name', instance.author_name)
         instance.save()
         return instance
 
     class Meta:
         model = News
-        fields = ('pk', 'author_name', 'content', 'created')
+        fields = ('pk', 'title', 'link', 'author_name', 'votes', 'created')
