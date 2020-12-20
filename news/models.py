57cd63e09e5129ba377a18f15bb3ded90cd7d103
Ivan Holikov <laurence4312@gmail.com>
2020-12-20 15:57:26 +0200

    Add Meta class to News model.

--- news/models.py
+++ news/models.py
@@ -6,3 +6,6 @@ class News(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     author_name = models.CharField(max_length=150, blank=False, default='')
     content = models.CharField(max_length=250, blank=True, default='')
+
+    class Meta:
+        ordering = ('created',)
